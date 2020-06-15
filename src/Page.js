f9bf774d09b8f8ce0306ffcc3a1beb2078fa3447
zhuxiuwu <1453855454@qq.com>
2020-06-15 17:13:19 +0800

    Page.js页面注释需要添加的功能

--- src/Page.js
+++ src/Page.js
@@ -4,7 +4,8 @@ import NotFound from './components/pages/NotFound';
 import Login from './components/pages/Login';
 import App from './App';
 import history from './utils/history'
-
+////需要出离当用户登陆后我们将login重定向到首页，放置用户在登陆状态下，通过修改url直接跳转到登陆界面中去
+////在404界面我们也要做相应的处理判断用户是否登陆，如果登陆将其重定向到主页，如果没有登陆重定向到登陆界面中去
 export default () => (
     <Router history={history}>
         <Switch>
